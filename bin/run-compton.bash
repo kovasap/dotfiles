b5a61b32ff6b9a995f9e4506848d0fb8280baf90
Kovas Palunas <kovas.palunas@gmail.com>
2020-01-13 12:06:34 -0800

    Transparency!

--- bin/run-compton.bash
+++ bin/run-compton.bash
@@ -1,1 +1,6 @@
-compton --glx-prog-win-rule ~kovas/compton/compton-chg-saturate-brightness-contrast.glsl:'class_g="Google-chrome"' --backend glx -b
+# See https://github.com/chjj/compton/blob/master/compton.sample.conf for
+# examples.
+compton --glx-prog-win-rule ~kovas/compton/compton-chg-saturate-brightness-contrast.glsl:'class_g="Google-chrome"' --backend glx -b \
+    --inactive-opacity 0.9 --active-opacity 0.95 \
+    # --blur-background --blur-kern "3x3box"
+
