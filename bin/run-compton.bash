10cc34de0dc76036d6f6a4d7ca493732d642d57d
Kovas Palunas <kovas.palunas@gmail.com>
2021-01-25 09:22:37 -0800

    updated compton settings and atom config

--- bin/run-compton.bash
+++ bin/run-compton.bash
@@ -3,7 +3,11 @@
 compton \
     --glx-prog-win-rule ~kovas/compton/compton-chg-saturate-brightness-contrast.glsl:'!class_g="kitty"' \
     --backend glx -b \
-    --inactive-opacity 0.9 --active-opacity 0.95 \
-    --opacity-rule 99:'class_g = "Godot" || class_g = "Google-chrome" || class_g = "Aseprite" || class_g = "StardewValley.bin.x86_64" || class_g = "StardewModdingAPI.bin.x86_64"'
+    --opacity-rule 93:'class_g = "kitty"' \
+    --inactive-dim 0.1
+    #  && !class_g="Atom"
+    #  && !class_g="Tabletop Simulator.x86_64"
+    # --inactive-opacity 0.9 --active-opacity 0.95 \
+    # --opacity-rule 99:'class_g = "Godot" || class_g = "Google-chrome" || class_g = "Aseprite" || class_g = "StardewValley.bin.x86_64" || class_g = "StardewModdingAPI.bin.x86_64"'
     # --blur-background --blur-kern "3x3box"
 
