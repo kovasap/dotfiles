04f0275aa12724e02fe003f471c805cd71b725e0
Kovas Palunas <kovas.palunas@gmail.com>
2021-03-07 17:49:38 -0800

    html 2 space indent and udev rules for mouse plug in

---
+++ udev_rules/fix_mouse_sens.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This script runs in the background when given no arguments, otherwise runs
+# normally.  See https://unix.stackexchange.com/questions/612686/udev-not-running-xinput-command-inside-script
+if [[ $1 ]]
+then
+  echo foreground >> /tmp/testt.log
+  xenv="env DISPLAY=:0 XAUTHORITY=/home/kovas/.Xauthority"
+  $xenv /usr/bin/xinput --set-prop 'pointer:Razer Razer DeathAdder Essential White Edition' 179 0.4 0.0 0.0 0.0 0.4 0.0 0.0 0.0 1.0
+else
+  echo background >> /tmp/testt.log
+  /usr/local/bin/fix_mouse_sens.bash an_argument &
+fi
