0a9bf7bcda462c22b9adb0c9a828177eeb388a5a
Kovas Palunas <kovas.palunas@gmail.com>
2021-10-11 15:34:10 -0700

    completion updates

--- .vim/init.lua
+++ .vim/init.lua
@@ -471,6 +471,11 @@ com! DiffSaved call g:DiffWithSaved()
 
 paq {'rafamadriz/friendly-snippets'}
 paq {'hrsh7th/cmp-buffer'}
+paq {'hrsh7th/cmp-emoji'}
+paq {'hrsh7th/cmp-calc'}
+paq {'hrsh7th/cmp-path'}
+paq {'hrsh7th/cmp-nvim-lua'}
+paq {'f3fora/cmp-spell'}
 paq {'hrsh7th/cmp-nvim-lsp'}
 paq {'hrsh7th/cmp-vsnip'}
 paq {'hrsh7th/vim-vsnip'}
@@ -485,12 +490,6 @@ cmp.setup({
     expand = function(args)
       -- For `vsnip` user.
       vim.fn["vsnip#anonymous"](args.body)
-
-      -- For `luasnip` user.
-      -- require('luasnip').lsp_expand(args.body)
-
-      -- For `ultisnips` user.
-      -- vim.fn["UltiSnips#Anon"](args.body)
     end,
   },
   mapping = {
@@ -499,10 +498,17 @@ cmp.setup({
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.close(),
     ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' }),
   },
   sources = {
     { name = 'nvim_lsp' },
     { name = 'vsnip' },
+    { name = 'emoji' },
+    { name = 'calc' },
+    { name = 'spell' },
+    { name = 'path' },
+    { name = 'nvim_lua' },
     { name = 'buffer',
       opts = {
         -- Complete from all open buffers, not just the current one.
@@ -520,76 +526,7 @@ cmp.setup({
 --   capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 -- }
 
--- vim.o.completeopt = "menuone,noselect"
--- require'compe'.setup {
---   enabled = true,
---   autocomplete = true,
---   debug = false,
---   min_length = 1,
---   preselect = 'enable',
---   throttle_time = 80,
---   source_timeout = 200,
---   incomplete_delay = 400,
---   max_abbr_width = 100,
---   max_kind_width = 100,
---   max_menu_width = 100,
---   documentation = true,
--- 
---   source = {
---     path = true,
---     buffer = true,
---     calc = true,
---     nvim_lsp = true,
---     nvim_lua = true,
---     vsnip = true,
---     emoji = true,
---     -- ultisnips = true,
---     spell = true,
---   },
--- }
--- local t = function(str)
---   return vim.api.nvim_replace_termcodes(str, true, true, true)
--- end
--- 
--- local check_back_space = function()
---     local col = vim.fn.col('.') - 1
---     if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
---         return true
---     else
---         return false
---     end
--- end
--- 
--- -- Use (s-)tab to:
--- --- move to prev/next item in completion menuone
--- --- jump to prev/next snippet's placeholder
--- _G.tab_complete = function()
---   if vim.fn.pumvisible() == 1 then
---     return t "<C-n>"
---   elseif vim.fn.call("vsnip#available", {1}) == 1 then
---     return t "<Plug>(vsnip-expand-or-jump)"
---   elseif check_back_space() then
---     return t "<Tab>"
---   else
---     return vim.fn['compe#complete']()
---   end
--- end
--- _G.s_tab_complete = function()
---   if vim.fn.pumvisible() == 1 then
---     return t "<C-p>"
---   elseif vim.fn.call("vsnip#jumpable", {-1}) == 1 then
---     return t "<Plug>(vsnip-jump-prev)"
---   else
---     -- If <S-Tab> is not working in your terminal, change it to <C-h>
---     return t "<S-Tab>"
---   end
--- end
--- map("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
--- map("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
--- map("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
--- map("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
 -- Integration with nvim-autopairs.
-
 _G.MUtils= {}  -- skip it, if you use another global object
 vim.g.completion_confirm_key = ""
 MUtils.completion_confirm=function()
