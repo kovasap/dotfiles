b76bb45cca31737fcc313353574ea2f266c66d23
Kovas Palunas <kovas.palunas@gmail.com>
2022-08-22 15:11:08 -0700

    bandcamp dl update

--- bin/bandcamp-dl-artist.bash
+++ bin/bandcamp-dl-artist.bash
@@ -1,6 +1,13 @@
 #!/bin/bash
-##sudo apt instal -y lynx && sudo pip3 install bandcamp-downloader##
-echo "Copy Bandacmp Url e.g https://ARTIST.bandcamp.com/music"
-read url
-lynx -dump -listonly -nonumbers $url | grep "bandcamp.com/album" > bandcamp-link.txt ; sleep 1
-for album in $(cat bandcamp-link.txt); do (bandcamp-dl -r --template "%{artist} - %{album}/%{track} - %{title}" $album); done
+# sudo apt instal -y lynx && sudo pip3 install bandcamp-downloader
+# Install https://github.com/prasmussen/gdrive/releases
+echo "First arg should be e.g https://ARTIST.bandcamp.com/music"
+rm -r bandcamp-music
+mkdir bandcamp-music
+cd bandcamp-music
+lynx -dump -listonly -nonumbers $1 | grep "bandcamp.com/album" > bandcamp-link.txt ; sleep 1
+for album in $(cat bandcamp-link.txt); do (bandcamp-dl -r --base-dir . --template "%{artist} - %{album}/%{track} - %{title}" $album); done
+rm bandcamp-link.txt
+
+# This is my "music" dir in Google Drive
+gdrive upload -r -p 12Xn6FKNZf6Mhv8v9BvjZcF4KilpdgRcj *
