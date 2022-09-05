9063e2dc18a92b8b6aa1dc40ff611da644fac3a5
Kovas Palunas <kovas.palunas@gmail.com>
2022-09-05 12:27:17 -0700

    clj #p tool description

--- .clojure/user.clj
+++ .clojure/user.clj
@@ -1,2 +1,3 @@
 (require 'hashp.core)
-#p (count "loaded")
+; https://github.com/weavejester/hashp/pull/1#issuecomment-731588467
+#p (count "Install https://github.com/gfredericks/user.clj in the project to use #p!")
