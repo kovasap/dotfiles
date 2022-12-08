d2f4b43611d517bde2adf15c6569b168ec851e33
Kovas Palunas <kovas.palunas@gmail.com>
2022-12-08 15:51:18 -0800

    fzf revert

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,13 +1,14 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */usr/local/google/home/kovas/.fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}/usr/local/google/home/kovas/.fzf/bin"
+if [[ ! "$PATH" == */home/kovas/.fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/home/kovas/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/usr/local/google/home/kovas/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/home/kovas/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/usr/local/google/home/kovas/.fzf/shell/key-bindings.zsh"
+source "/home/kovas/.fzf/shell/key-bindings.zsh"
+export FZF_DEFAULT_OPTS='--bind=ctrl-k:up,ctrl-e:down'
