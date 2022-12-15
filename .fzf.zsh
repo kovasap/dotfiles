b4fe7a8217f934461dd8c330068941794e6ddccf
Kovas Palunas <kovas.palunas@gmail.com>
2022-12-15 21:02:14 +0000

    home dir paths for better cross machine interop

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,14 +1,14 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/kovas/.fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}/home/kovas/.fzf/bin"
+if [[ ! "$PATH" == *.fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}~/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/kovas/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source ~/.fzf/shell/completion.zsh 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/kovas/.fzf/shell/key-bindings.zsh"
+source ~/.fzf/shell/key-bindings.zsh
 export FZF_DEFAULT_OPTS='--bind=ctrl-k:up,ctrl-e:down'
