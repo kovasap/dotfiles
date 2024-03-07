813d2be3f2a2c480a05f938a48764903775a06c5
Kovas Palunas <kovas.palunas@gmail.com>
2024-03-07 10:27:58 -0800

    fix for bandcamp redirect urls

--- bin/bandcamp-dl-artist.bash
+++ bin/bandcamp-dl-artist.bash
@@ -5,7 +5,7 @@ echo "First arg should be e.g https://ARTIST.bandcamp.com/music"
 rm -r bandcamp-music
 mkdir bandcamp-music
 cd bandcamp-music
-lynx -dump -listonly -nonumbers $1 | grep "bandcamp.com/album" > bandcamp-link.txt ; sleep 1
+lynx -dump -listonly -nonumbers $1 | grep "/album/" > bandcamp-link.txt ; sleep 1
 for album in $(cat bandcamp-link.txt); do (bandcamp-dl -r --base-dir . --template "%{artist} - %{album}/%{track} - %{title}" $album); done
 rm bandcamp-link.txt
 
