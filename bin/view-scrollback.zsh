25eb35e44b3ef63797a14199952d41ebd8384342
Kovas Palunas <kovas.palunas@gmail.com>
2024-07-26 14:07:03 -0700

    save scrollback in home dir

--- bin/view-scrollback.zsh
+++ bin/view-scrollback.zsh
@@ -3,9 +3,9 @@
 # Check if in tmux session or not
 if ! { [ "$TERM" = "screen" ] && [ -n "$TMUX" ]; } then
   tmux capture-pane -S -
-  tmux save-buffer scrollback.txt
+  tmux save-buffer ~/scrollback.txt
 else
-  kitty @ get-text > scrollback.txt
+  kitty @ get-text > ~/scrollback.txt
 fi
 
-nvim scrollback.txt
+nvim ~/scrollback.txt
