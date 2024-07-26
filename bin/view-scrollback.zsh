1a51d78e244206fca3eb50d4eff36428d726a774
Kovas Palunas <kovas.palunas@gmail.com>
2024-07-26 14:10:07 -0700

    save latest scrollback

--- bin/view-scrollback.zsh
+++ bin/view-scrollback.zsh
@@ -2,7 +2,11 @@
 
 # Check if in tmux session or not
 if ! { [ "$TERM" = "screen" ] && [ -n "$TMUX" ]; } then
-  tmux capture-pane -S -
+  # It's also possible to supply `-` instead of a number to -S for infinite
+  # capture, although sometimes this leads to truncation anyway.  I've found
+  # it's better to just capture the last 10000 lines like this to avoid loosing
+  # some of the most recent lines.
+  tmux capture-pane -S -10000
   tmux save-buffer ~/scrollback.txt
 else
   kitty @ get-text > ~/scrollback.txt
