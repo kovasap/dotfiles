c519cb209009cd51d4e7161ae7c0a1e22951b2b4
Kovas Palunas <kovas.palunas@gmail.com>
2025-05-06 12:48:42 -0700

    still not working keyboard connect

--- bin/connect-to-keyboard.bash
+++ bin/connect-to-keyboard.bash
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-for MAC in 'F9:32:58:CE:C8:8B' 'D9:83:07:36:6D:C9'
-do
-    echo 'trying to connect to ' $MAC
-    # bluetoothctl disconnect $MAC
-    # bluetoothctl untrust $MAC
-    # bluetoothctl remove $MAC
-    sleep 2
-    bluetoothctl trust $MAC
-    bluetoothctl pair $MAC
-    bluetoothctl connect $MAC
-done
+bt-device -c Lintilla
+bt-device -c Logi M650 L B
+
