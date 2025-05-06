b85425bb94ae8ec59d4353a83f971843a80d720d
Kovas Palunas <kovas.palunas@gmail.com>
2025-05-06 12:35:29 -0700

    keyboard connect still broken

--- bin/connect-to-keyboard.bash
+++ bin/connect-to-keyboard.bash
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-MAC='C6:4D:EC:B5:DA:B3'
-bluetoothctl disconnect $MAC
-bluetoothctl untrust $MAC
-bluetoothctl remove $MAC
-sleep 3
-bluetoothctl connect $MAC
-bluetoothctl pair $MAC
+for MAC in 'F9:32:58:CE:C8:8B' 'D9:83:07:36:6D:C9'
+do
+    echo 'trying to connect to ' $MAC
+    # bluetoothctl disconnect $MAC
+    # bluetoothctl untrust $MAC
+    # bluetoothctl remove $MAC
+    sleep 2
+    bluetoothctl trust $MAC
+    bluetoothctl pair $MAC
+    bluetoothctl connect $MAC
+done
