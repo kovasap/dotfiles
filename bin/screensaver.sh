05f3ba1c906c75d900a7aee8a6d833933e3ea964
Kovas Palunas <kovas.palunas@gmail.com>
2025-05-27 20:56:47 -0700

    turn off screens working

--- bin/screensaver.sh
+++ bin/screensaver.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 if [[ $(hostname) == 'frostyarch' ]]; then
+  # See https://wiki.archlinux.org/title/Display_Power_Management_Signaling#Runtime_settings
+  sleep 1
   xset dpms force standby
 else
   /usr/share/goobuntu-desktop-files/xsecurelock.sh
