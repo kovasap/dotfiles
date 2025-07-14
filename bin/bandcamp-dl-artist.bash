7622c9e6d6aa29066f05754f6e55cfe321ccc363
Kovas Palunas <kovas.palunas@gmail.com>
2025-07-14 15:26:29 -0700

    get tracks as well as albums

--- bin/bandcamp-dl-artist.bash
+++ bin/bandcamp-dl-artist.bash
@@ -8,8 +8,8 @@ cd bandcamp-music
 # This gets stuff most of the time, but not always everything
 # lynx -dump -listonly -nonumbers $1 | grep "/album/" > bandcamp-link.txt ; sleep 1
 curl "$1/music" | grep -oPi "/album/[^&?'\"\s]+" | sed "s~^~$1~" > bandcamp-link.txt
+curl "$1/music" | grep -oPi "/track/[^&?'\"\s]+" | sed "s~^~$1~" >> bandcamp-link.txt
 cat bandcamp-link.txt
-sleep 1
 for album in $(cat bandcamp-link.txt); do (bandcamp-dl -r --base-dir . --template "%{artist} - %{album}/%{track} - %{title}" $album); done
 rm bandcamp-link.txt
 
