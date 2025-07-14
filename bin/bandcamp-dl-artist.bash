0505d5a5870442386b38ddc0333e0849eeb6a15f
Kovas Palunas <kovas.palunas@gmail.com>
2025-07-14 15:05:07 -0700

    better bandcamp dl script

--- bin/bandcamp-dl-artist.bash
+++ bin/bandcamp-dl-artist.bash
@@ -1,11 +1,15 @@
 #!/bin/bash
 # sudo apt instal -y lynx && sudo pip3 install bandcamp-downloader
 # Install https://github.com/prasmussen/gdrive/releases
-echo "First arg should be e.g https://ARTIST.bandcamp.com/music"
+echo "First arg should be e.g https://ARTIST.bandcamp.com"
 rm -r bandcamp-music
 mkdir bandcamp-music
 cd bandcamp-music
-lynx -dump -listonly -nonumbers $1 | grep "/album/" > bandcamp-link.txt ; sleep 1
+# This gets stuff most of the time, but not always everything
+# lynx -dump -listonly -nonumbers $1 | grep "/album/" > bandcamp-link.txt ; sleep 1
+curl "$1/music" | grep -oPi "/album/[^&?'\"\s]+" | sed "s~^~$1~" > bandcamp-link.txt
+cat bandcamp-link.txt
+sleep 1
 for album in $(cat bandcamp-link.txt); do (bandcamp-dl -r --base-dir . --template "%{artist} - %{album}/%{track} - %{title}" $album); done
 rm bandcamp-link.txt
 
