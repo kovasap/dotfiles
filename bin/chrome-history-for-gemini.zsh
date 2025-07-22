152ed7166d0f551bd57e8826721e01652f84a5ee
Kovas Palunas <kovas.palunas@gmail.com>
2025-07-21 20:07:17 -0700

    chrome history analysis

---
+++ bin/chrome-history-for-gemini.zsh
@@ -0,0 +1,24 @@
+# Supply arg to script for which numbered chrome profile to search.
+c () {
+  if [ $# -eq 0 ]
+    then
+      cp -f ~/.config/google-chrome/Default/History /tmp/h
+    else
+      cp -f ~/.config/google-chrome/Profile\ $1/History /tmp/h
+  fi
+
+  sqlite3 /tmp/h "SELECT
+  urls.title
+FROM
+  urls
+WHERE
+  urls.last_visit_time >= (
+    (CAST(strftime('%s', 'now', '-30 days') AS INTEGER) + 11644473600) * 1000000
+  )
+ORDER BY
+  urls.last_visit_time DESC;" > ~/history_titles.txt
+
+  gemini -p "Summarize the web activity in the ~/history_titles.txt file."
+}
+
+c $1
